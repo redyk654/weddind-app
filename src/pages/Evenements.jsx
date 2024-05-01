62123e208be8c1b10fe8e37c38ac8ca46b98bb0d
user <redyk654@gmail.com>
2024-05-01 01:12:40 +0100

    display event: in progress (display on card)

--- src/pages/Evenements.jsx
+++ src/pages/Evenements.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet, useNavigate, useParams } from 'react-router-dom'
 import { Button } from '@mui/material'
 import useAuth from '../hooks/useAuth'
 import AddIcon from '@mui/icons-material/Add'
+import firebase from '../firebase'
 
 export default function Evenements() {
     const { eventId } = useParams();
     const navigate = useNavigate();
     const { user } = useAuth();
+
+    const [eventsList, setEventsList] = useState([])
+
+    useEffect(() => {
+      if (user) {
+        fecthEvents()
+      }
+    }, [user])
+
+    const fecthEvents = async () => {
+      try {
+        const db = firebase.firestore();
+        const eventsRef = db.collection('evenements');
+        const snapshot = await eventsRef.where('userId', '==', user.uid).get();
+        if (snapshot.empty) {
+            console.log('No matching documents.');
+            return;
+        }
+        const tempList = []
+        snapshot.forEach(doc => {
+            tempList.push(doc.data())
+        });
+        setEventsList(tempList)
+      } catch (error) {
+        console.error("Erreur lors de la récupération des évènements", error)
+      }
+    }
+
 
     if (eventId) {
         return (
